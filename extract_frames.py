9d85370e61523f7b25b7dc6846852dc8577727f0
Gregor Schulte <bzesg01@uni-tuebingen.de>
2019-08-26 13:47:00 +0200

    almost done

--- extract_frames.py
+++ extract_frames.py
@@ -12,21 +12,11 @@ frameshift = 0
 temppath = "empty"
 tempfile = "empty"
 
+multiplier = 0
+restframes = 0
+
 for file in glob('**/*.mp4'):
     
-    if os.path.dirname(file) == temppath: 
-        print ("altes  Verzeichis")
-        if os.path.abspath(file) == tempfile:
-            frameshift +=1
-            print("same File")
-        else:
-            print("new File")
-
-        
-    else:
-        print("neues Verzeichnis")
-        framecounter = 0
-        frameshift = 0
     
     print("Current file: " , os.path.abspath(file))
     
@@ -36,15 +26,13 @@ for file in glob('**/*.mp4'):
     
     success,image = vidcap.read()
     
-    seconds = 25
+    seconds = 40
     
     fps = vidcap.get(cv2.CAP_PROP_FPS) # Gets the frames per second
     
     frames = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
     
     multiplier = fps * seconds
-    
-    restframes = frames % multiplier
     
     
     print("Frames:\t\t",frames, 
@@ -53,21 +41,42 @@ for file in glob('**/*.mp4'):
           "\nMultiplier:\t", multiplier , 
           "\nFrames left:\t", restframes )
     
+    
+    
+    if os.path.dirname(file) == temppath: 
+        print ("altes  Verzeichis")
+        if os.path.abspath(file) == tempfile:
+            print("same File")
+        else:
+            print("new File")
+            frameshift = frameshift + restframes
+        
+    else:
+            frameshift = 0
+            print("neues Verzeichnis")
+            framecounter = 0
+
+    
 
     
     print ("\n\n start writing frames \n\n ")
     while success:
-        frameId = int(round(vidcap.get(1))) #current frame number, rounded b/c sometimes you get frame intervals which aren't integers...this adds a little imprecision but is likely good enough
+        frameId = int(round(vidcap.get(1)))  #current frame number, rounded b/c sometimes you get frame intervals which aren't integers...this adds a little imprecision but is likely good enough
+        position = frameId + frameshift
         success, image = vidcap.read()
     
-        if frameId % multiplier == 0:
+        if (position) % multiplier == 0:
             framecounter += 1
             filename = os.path.abspath(file).replace('.mp4','') +"_"+ str(frameId).zfill(8) + "_"+str(framecounter).zfill(8)+".jpg"
             print (filename)
             cv2.imwrite(filename, image)
+            
 
 
     print ("\n\n finished wrinting frames \n\n")
+    restframes = (frames - frameshift) % multiplier
+    #frameshift = multiplier - restframes
+ 
     temppath = os.path.dirname(file)
     tempfile = os.path.abspath(file)
     vidcap.release()
