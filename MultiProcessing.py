3c452557f199b002ff444569d16c5adb6a22f7b3
Gregor Schulte <bzesg01@uni-tuebingen.de>
2019-09-17 12:40:05 +0200

    include ffmpeg

--- MultiProcessing.py
+++ MultiProcessing.py
@@ -5,12 +5,37 @@ import shutil
 
 from glob import glob
 
-filelist = glob("C:\\csv\\input\\*.csv", recursive=True)
+import struct 
 
-def f(x):
-    print(x)
-    cp = shutil.copy(x,os.path.dirname(os.path.dirname(os.path.abspath(x))))
-    return cp
+
+if (struct.calcsize('P')*8== 64):
+    print('64 bit system recognized')
+    ffmpegpath = 'C:\\ffmpeg64\\bin\\ffmpeg.exe'
+else: 
+    print('64 bit system recognized')
+    ffmpegpath = 'C:\\ffmpeg32\\bin\\ffmpeg.exe'
+    
+parameters = ' -an -vcodec copy '
+
+
+
+    
+
+filelist = glob("C:\\test\\input\\*.mp4", recursive=True)
+
+#def f(x):
+#    print(x)
+#    cp = shutil.copy(x,os.path.dirname(os.path.dirname(os.path.abspath(x))))
+#    return cp
+
+
+def convert_and_delete(file):
+    filename = str(os.path.abspath(file))
+    mutename = str(os.path.abspath(file)[:-4]+'_m'+os.path.splitext(file)[1])
+    p_string = ffmpegpath + ' -i ' + filename + parameters + mutename
+    print(p_string)    
+    #c = subprocess.call(p_string)
+    #os.remove(file) 
 
 if __name__ == '__main__':
     with Pool(multiprocessing.cpu_count()) as p:
